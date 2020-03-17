2de02bb3395a441c2c527cb0a2c0c69a0b23bfae
makarai <17119942+ma-karai@users.noreply.github.com>
2020-03-17 12:08:10 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo what is going on
 
 # Install the binary package
 #pkg add --force https://github.com/MediaBrowser/Emby.Releases/releases/download/4.4.0.27/emby-server-freebsd11_4.4.0.27_amd64.txz
